fb0272f69dfbf239f88546c606fa0b50be7c2ff6
Umair2231 <jafri223@gmail.com>
2025-12-18 21:36:04 +0000

    updating the provider info

--- terraformut_implicit.tf
+++ terraformut_implicit.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.44.0"
+      version = "4.57.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "aa50dcb0-04de-41e4-bb3e-260e54d18bec"
+  subscription_id = "8b0422c9-d3b4-4ad5-b676-1cd162a61f87"
 }
 
 # 2 resource groups 
@@ -26,13 +26,13 @@ resource "azurerm_resource_group" "rg2" {
 
 # 2 storage account blocks
 
-resource "azurerm_storage_account" "stg1" {
-  name                     = "storageactut1"
-  resource_group_name      = azurerm_resource_group.rg1.name
-  location                 = azurerm_resource_group.rg1.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
+# resource "azurerm_storage_account" "stg1" {
+#   name                     = "storageactut1"
+#   resource_group_name      = azurerm_resource_group.rg1.name
+#   location                 = azurerm_resource_group.rg1.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+# }
 
 # resource "azurerm_storage_account" "stg2" {
 #   name                     = "storageactut2"
